5933e48f6cc1750e8cb716be595930a5ee51e999
Oliver Alexander <oliveralexander@Olivers-MacBook-Pro.local>
2016-12-06 14:48:54 -0600

    Initial Commit - Server up and running, files connected

---
+++ server/app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// serve static files
+app.use(express.static(path.resolve('./server/public')));
+
+
+
+// server index file
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, './public/views/index.html'));
+});
+
+app.listen(3000, function() {
+  console.log("server running, check localhost:3000");
+});
