ccd86d338edebd615861945ef33c026c4ddf2f0b
Oliver Alexander <oliveralexander@Olivers-MacBook-Pro.local>
2016-12-06 16:03:48 -0600

    Add templates and routes

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,38 @@
-console.log('working!')
+var app = angular.module('pivotApp', ['ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+  .when('/home', {
+    templateUrl: '/views/templates/home.html',
+    controller: 'HomeController',
+    controllerAs: 'home'
+  })
+  .when('/progress', {
+    templateUrl: '/views/templates/progress.html',
+    controller: 'ProgressController',
+    controllerAs: 'prog'
+  })
+  .when('/verbs', {
+    templateUrl: '/views/templates/verbs.html',
+    controller: 'VerbsController',
+    controllerAs: 'verbs'
+  })
+  .when('/choice', {
+    templateUrl: '/views/templates/choose.game.html',
+    controller: 'ChoiceController',
+    controllerAs: 'choice'
+  })
+  .when('/definition', {
+    templateUrl: '/views/templates/definition.html',
+    controller: 'DefinitionController',
+    controllerAs: 'def'
+  })
+  .when('/blank', {
+    templateUrl: '/views/templates/blank.html',
+    controller: 'BlankController',
+    controllerAs: 'blank'
+  })
+  .otherwise({
+    redirectTo: 'home'
+  });
+}]);
