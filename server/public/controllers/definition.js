feb986cb3df44fa29ec9d70aee85727a179a421e
Oliver Alexander <oliveralexander@Olivers-MacBook-Pro.local>
2016-12-07 14:26:25 -0600

    Basic defintion game logic works. Contains duplicates

--- server/public/controllers/definition.js
+++ server/public/controllers/definition.js
@@ -1,10 +1,14 @@
 app.controller("DefinitionController", ["$http", function($http){
 
   var self = this;
-  var GAME_VERBS_NUMBER = 10;
+  var GAME_VERBS = 10;
+  var GUESS_OPTIONS = 4;
   self.databaseVerbs = [];
   self.gameVerbs = [];
-  self.currentVerb = {};
+  self.currentVerbDefinition = "";
+  self.currentVerb = "";
+  self.guessOptions = [];
+  self.uniquePhrasalVerbs = ['bring up', 'give in', 'stay up', 'go out', 'look into'];
 
     getVerbs();
 
@@ -17,16 +21,33 @@ app.controller("DefinitionController", ["$http", function($http){
     }
 
     self.getCurrentVerb = function(){
-      self.currentVerb = self.gameVerbs.pop();
-      console.log(self.currentVerb);
+      var currentVerbObject = self.gameVerbs.pop();
+      self.currentVerbDefinition = currentVerbObject.definition;
+      self.currentVerb = currentVerbObject.phrasal_verb;
+      assignGuessOptions();
+
     };
 
+    function assignGuessOptions(){
+      self.guessOptions = [];
+      var correctAnswerPosition = randomNumber(0, GUESS_OPTIONS-1);
+      for (var i = 0; i < GUESS_OPTIONS; i++) {
+        if(i == correctAnswerPosition){
+          self.guessOptions.push(self.currentVerb);
+        } else{
+          var wrongVerb = self.uniquePhrasalVerbs[randomNumber(0, self.uniquePhrasalVerbs.length-1)];
+          self.guessOptions.push(wrongVerb);
+        }
+      }
+      console.log(self.guessOptions);
+    }
+
     function addVerbsToGame(){
-      for (var i = 0; i < GAME_VERBS_NUMBER ; i++) {
+      for (var i = 0; i < GAME_VERBS ; i++) {
         var verb = self.databaseVerbs[randomNumber(0, self.databaseVerbs.length-1)];
         //console.log(verb.phrasal_verb, "=", verb.definition);
         self.gameVerbs.push(verb);
-        console.log(self.gameVerbs);
+        //console.log(self.gameVerbs);
       }
     }
 
