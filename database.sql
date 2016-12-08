b2061da0bea42c6748f1e19bcb18016ea1ef78d4
Oliver Alexander <oliveralexander@Olivers-MacBook-Pro.local>
2016-12-08 11:39:29 -0600

    Add more test data to DB, functioning correctly

--- database.sql
+++ database.sql
@@ -23,3 +23,16 @@ VALUES ('break up', 'break', 'up', 'To come to an end (marriage, relationship)')
 ('get up', 'get', 'up', 'To rise, leave bed'),
 ('make up', 'make', 'up', 'To invent (excuse, story)'),
 ('make up', 'make', 'up', 'To reconcile');
+
+INSERT INTO phrasal_verbs (phrasal_verb, base, preposition, definition)
+VALUES ('hold on', 'hold', 'on', 'To wait'),
+('hold on', 'hold', 'on', 'To grip tightly'),
+('put off', 'put', 'off', 'To postpone, arrange a later date'),
+('put on', 'put', 'on', 'To turn on, switch on'),
+('put out', 'put', 'out', 'To extinguish'),
+('point out', 'point', 'out', 'To indicate/direct attention to something'),
+('run out', 'run', 'out', 'To have no more of something.'),
+('set up', 'set', 'up', 'To start a business'),
+('show off', 'show', 'off', 'To brag or want to be admired'),
+('wear out', 'wear', 'out', 'To become unusable'),
+('wear out', 'wear', 'out', 'To become very tired');
