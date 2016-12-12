d42789e9515cb8ca06748a5d5b78294dad9f25f9
Oliver Alexander <oliveralexander@Olivers-MacBook-Pro.local>
2016-12-12 11:40:58 -0600

    Add firebase configuration

---
+++ server/public/scripts/configuration/config.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+var config = {
+apiKey: "AIzaSyD8n5n2mJ_qEzEQhW1gTc65ukLuvftcg24",
+authDomain: "pivot-90277.firebaseapp.com",
+databaseURL: "https://pivot-90277.firebaseio.com",
+storageBucket: "pivot-90277.appspot.com",
+messagingSenderId: "159126597362"
+};
+firebase.initializeApp(config);
