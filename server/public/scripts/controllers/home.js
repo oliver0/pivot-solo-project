5573c46a11d16ed08b155141273f5095296dbb82
Oliver Alexander <oliveralexander@Olivers-MacBook-Pro.local>
2016-12-12 12:00:11 -0600

    Add AuthFactory

--- server/public/scripts/controllers/home.js
+++ server/public/scripts/controllers/home.js
@@ -1,5 +1,15 @@
-app.controller("HomeController", ["$http", function($http){
+app.controller("HomeController", ["$http", "AuthFactory", function($http, AuthFactory){
 
   console.log("HomeControler running");
+
+  var currentUser;
+
+  self.logIn = function(){
+    AuthFactory.logIn();
+  }
+
+  self.logOut = function(){
+    AuthFactory.logOut();
+  }
 
   }]);
---
+++ server/public/scripts/services/auth.factory.js
@@ -0,0 +1,50 @@
+app.factory('AuthFactory', ["$http", function($http, $firebaseAuth) {
+
+  var auth = $firebaseAuth();
+  var self = this;
+  var currentUser;
+
+  // This code runs whenever the user logs in
+  function logIn(){
+    auth.$signInWithPopup("google").then(function(firebaseUser) {
+      currentUser = firebaseUser;
+      console.log(currentUser);
+      console.log("Firebase Authenticated as: ", firebaseUser.user.displayName);
+    }).catch(function(error) {
+      console.log("Authentication failed: ", error);
+    });
+  };
+
+  // This code runs whenever the user changes authentication states
+  // e.g. whevenever the user logs in or logs out
+  //
+  auth.$onAuthStateChanged(function(firebaseUser){
+    // firebaseUser will be null if not logged in
+    if(firebaseUser) {
+      console.log(firebaseUser);
+    } else {
+      console.log('Not logged in or not authorized.');
+    }
+
+  });
+
+  // This code runs when the user logs out
+  function logOut (){
+    auth.$signOut().then(function(){
+      console.log('Logging the user out!');
+    });
+  };
+
+  var userData = {
+    logIn: function() {
+      return logIn();
+    },
+    logOut: function() {
+      return logIn();
+    },
+    currentUser: function(){
+      return currentUser;
+    }
+  }
+
+}]);
