c7bedc5404389e0bd0ed01407db56a29cbb502ef
Oliver Alexander <oliveralexander@Olivers-MacBook-Pro.local>
2016-12-13 13:38:10 -0600

    Reach users route - email undefined error

--- server/public/scripts/services/auth.factory.js
+++ server/public/scripts/services/auth.factory.js
@@ -8,46 +8,35 @@ app.factory('AuthFactory', ["$http", "$firebaseAuth", function($http, $firebaseA
   // This code runs whenever the user logs in
   function logIn(){
     auth.$signInWithPopup("google").then(function(firebaseUser) {
-      currentUser = firebaseUser;
-      console.log('currentUser:',currentUser);
-      if(currentUser) {
-        firebaseUser.getToken().then(function(idToken){
-          console.log('ID TOKEN:', idToken)
-          $http({
-            method: 'GET',
-            url: '/users',
-            headers: {
-              id_token: idToken
-            }
-          });
-        });
-      } else {
-        console.log('Not logged in or not authorized.');
-      }
+      console.log(firebaseUser.user.displayName);
     }).catch(function(error) {
       console.log("Authentication failed: ", error);
     });
     return
-
-  };
+};
 
   // This code runs whenever the user changes authentication states
   // e.g. whevenever the user logs in or logs out
   //
-  // auth.$onAuthStateChanged(function(firebaseUser){
-  //   // firebaseUser will be null if not logged in
-  //   if(firebaseUser) {
-  //     firebaseUser.getToken().then(function(idToken){
-  //       $http({
-  //         method: 'GET',
-  //         url: '/'
-  //       })
-  //     })
-  //
-  //   } else {
-  //     console.log('Not logged in or not authorized.');
-  //   }
-  // });
+  auth.$onAuthStateChanged(function(firebaseUser){
+    // firebaseUser will be null if not logged in
+    currentUser = firebaseUser;
+    console.log('currentUser:',currentUser);
+    if(currentUser) {
+      firebaseUser.getToken().then(function(idToken){
+        console.log('ID TOKEN:', idToken)
+        $http({
+          method: 'GET',
+          url: '/users',
+          headers: {
+            id_token: idToken
+          }
+        });
+      });
+    } else {
+      console.log('Not logged in or not authorized.');
+    }
+  });
 
   // This code runs when the user logs out
   function logOut (){
