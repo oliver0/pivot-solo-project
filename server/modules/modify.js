ec60c0d45c9ce9923cb17bd9dc99edfe1f633497
Oliver Alexander <oliveralexander@Olivers-MacBook-Pro.local>
2016-12-28 14:30:08 -0600

    Modify modifyVerbList function

--- server/modules/modify.js
+++ server/modules/modify.js
@@ -10,7 +10,7 @@ function modifyVerbList(verbs, numVerbs) {
   var lowPercentNumber = Math.round(numVerbs/2);
   for (var i = 0; i < numVerbs - 1; i++) {
     var lowPercent = Math.round(verbs.length / 2);
-    if (i < lowPercentNumber) {
+    if (i < lowPercentNumber+1) {
       verb = verbs.splice(random(0, lowPercent - 1), 1);
       gameVerbs.push(verb[0]);
     } else {
