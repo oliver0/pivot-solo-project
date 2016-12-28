45c5a876adc41851a994e42fbb89c98340c60247
Oliver Alexander <oliveralexander@Olivers-MacBook-Pro.local>
2016-12-28 12:04:41 -0600

    Modify module returns gameVerbs correctly

--- server/modules/modify.js
+++ server/modules/modify.js
@@ -1,12 +1,22 @@
 var random = require('../modules/random');
 
 function modifyVerbList(verbs) {
+  var verb;
   var gameVerbs = [];
-  gameVerbs.push(verbs[random(0, 1)]);
-  // for (var i = 0; i < verbs.length; i++) {
-  //   gameVerbs.push(verbs[i].percentage);
-  // }
+  var oneOfWOrstTwo = verbs.splice(random(0, 1), 1);
+  gameVerbs.push(oneOfWOrstTwo);
 
+
+  for (var i = 0; i < 9; i++) {
+    var lowPercent = Math.round(verbs.length / 2);
+    if (i < 5) {
+      verb = verbs.splice(random(0, lowPercent - 1), 1);
+      gameVerbs.push(verb);
+    } else {
+      verb = verbs.splice(random(lowPercent, verbs.length - 1), 1);
+      gameVerbs.push(verb);
+    }
+  }
   return gameVerbs;
 }
 
