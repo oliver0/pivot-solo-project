03a6fd6a3a91d9504c63ed89091c469986a0a6cf
Oliver Alexander <oliveralexander@Olivers-MacBook-Pro.local>
2016-12-28 11:30:23 -0600

    Add module. Add worst or 2nd worst verb to gameVerbs

--- server/modules/modify.js
+++ server/modules/modify.js
@@ -1,9 +1,13 @@
-function modifyVerbList(verbs){
-  var percentages = [];
-  for (var i = 0; i < verbs.length; i++) {
-    percentages.push(verbs[i].percentage);
-  }
-  return percentages;
+var random = require('../modules/random');
+
+function modifyVerbList(verbs) {
+  var gameVerbs = [];
+  gameVerbs.push(verbs[random(0, 1)]);
+  // for (var i = 0; i < verbs.length; i++) {
+  //   gameVerbs.push(verbs[i].percentage);
+  // }
+
+  return gameVerbs;
 }
 
 module.exports = modifyVerbList;
